5177d5db34ca5c3dcfe5842905a22458577546a5
wglsky <40151406+wglsky@users.noreply.github.com>
2018-12-12 15:36:34 +0100

    Firstcommit

---
+++ HelloWorldFromGitHub/src/main/java/com/wglsky/HelloWorldFromGitHubApplication.java
@@ -0,0 +1,14 @@
+package com.wglsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldFromGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldFromGitHubApplication.class, args);
+	}
+
+}
+
---
+++ HelloWorldFromGitHub/src/test/java/com/wglsky/HelloWorldFromGitHubApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wglsky;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldFromGitHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
